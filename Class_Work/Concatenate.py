88cedfe8b2a54eddd43c1a156cfc20c7eb14d4b7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-09-01 11:45:04 +0530

    Source Code

---
+++ Class_Work/Concatenate.py
@@ -0,0 +1,6 @@
+#Concatenating lists : We can concatenate two lists using (+) operator
+list1 = [1, 2, 3, "Ankit"]
+list2 = [4, 5, "Sharma"]
+#list3 = list1 + list2
+print(list1+list2)
+#print(list3)
