39b5aea4955269c7378d20429f7cbdf9fc6b83d3
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-12-24 15:58:39 +0530

    Assiment

---
+++ Python/Chapter_2/FindLength.py
@@ -0,0 +1,2 @@
+name = input("Enter name : ")
+print("length of you name is :- ", len(name))
---
+++ Python/Chapter_2/Occurrence.py
@@ -0,0 +1,2 @@
+str = "Hi, $I am the $ symbol $99.99"
+print("Count the occurrence of $ is :- ",str.count("$"))
