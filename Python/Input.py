7f0de16bf4523dde07e2f4e842d51f5c09660cd8
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-12-24 14:20:55 +0530

    Input

---
+++ Python/Input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter age : "))
+marks = float(input("Enter marks : "))
+
+print("Welcome ",name)
+print("Age =",age)
+print("marks =",marks)
---
+++ Python/TypeConversion.py
@@ -0,0 +1,15 @@
+#manuall
+a = 2
+b = 4.25
+
+sum = a + b # 2.0 + 4.25 => 6.25
+
+print(sum)
+
+#Type Casting
+#manuall
+c = int(b)
+
+sum = a + c # 2.0 + 4.25 => 6.25
+
+print(sum)
